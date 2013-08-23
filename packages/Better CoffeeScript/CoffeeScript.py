66ad7c49b6fd7f875e065176a9a202931c5c560a
Michael <obrocnikm@gmail.com>
2013-08-23 23:48:36 +0200

    Fix Better Coffeescript

---
+++ packages/Better CoffeeScript/CoffeeScript.py
@@ -0,0 +1,475 @@
+#aponxi v0.6
+import sublime
+import sys
+from os import path
+import os
+from subprocess import Popen, PIPE
+from sublime_plugin import TextCommand
+from sublime_plugin import WindowCommand
+import sublime_plugin
+import time
+import functools
+import sys
+import locale
+
+def settings_get(name, default=None):
+    # load up the plugin settings
+    plugin_settings = sublime.load_settings('CoffeeScript.sublime-settings')
+    # project plugin settings? sweet! no project plugin settings? ok, well promote plugin_settings up then
+    if sublime.active_window() and sublime.active_window().active_view():
+        project_settings = sublime.active_window().active_view().settings().get("CoffeeScript")
+    else:
+        project_settings = {}
+
+    # what if this isn't a project?
+    # the project_settings would return None (?)
+    if project_settings is None:
+        project_settings = {}
+        
+    setting = project_settings.get(name, plugin_settings.get(name, default))
+    return setting
+
+
+def run(cmd, args=[], source="", cwd=None, env=None):
+    if not type(args) is list:
+        args = [args]
+    if sys.platform == "win32":
+        args = [cmd] + args
+        if sys.version_info[0] == 2:
+            for i in range(len(args)):
+                args[i] = args[i].encode(locale.getdefaultlocale()[1])
+        proc = Popen(args, env=env, cwd=cwd, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell = True)
+        try:
+            stat = proc.communicate(input=source)
+        except:
+            stat = proc.communicate(input=source.encode("utf8"))
+    else:
+        if env is None:
+            env = {"PATH": settings_get('binDir', '/usr/local/bin')}
+
+        # adding custom PATHs from settings
+        customEnv = settings_get('envPATH', "")
+        if customEnv:
+            env["PATH"] = env["PATH"]+":"+customEnv
+        if source == "":
+            command = [cmd] + args
+        else:
+            command = [cmd] + args + [source]
+        proc = Popen(command, env=env, cwd=cwd, stdout=PIPE, stderr=PIPE)
+        stat = proc.communicate()
+    okay = proc.returncode == 0
+    return {"okay": okay, "out": stat[0].decode('utf-8'), "err": stat[1].decode('utf-8')}
+
+
+def brew(args, source, cwd=None):
+    if sys.platform == "win32":
+        args.append("-s")
+    else:
+        args.append("-e")
+
+    return run("coffee", args=args, source=source.encode('utf-8'))
+
+
+def cake(task, cwd):
+    return run("cake", args=task, cwd=cwd)
+
+
+def isCoffee(view=None):
+    if view is None:
+        view = sublime.active_window().active_view()
+    return 'source.coffee' in view.scope_name(0)
+
+
+class Text():
+    @staticmethod
+    def all(view):
+        return view.substr(sublime.Region(0, view.size()))
+
+    @staticmethod
+    def sel(view):
+        text = []
+        for region in view.sel():
+            if region.empty():
+                continue
+            text.append(view.substr(region))
+        return "".join(text)
+
+    @staticmethod
+    def get(view):
+        text = Text.sel(view)
+        if len(text) > 0:
+            return text
+        return Text.all(view)
+
+
+class CompileCommand(TextCommand):
+    def is_enabled(self):
+        return isCoffee(self.view)
+
+    def run(self, *args, **kwargs):
+        no_wrapper = settings_get('noWrapper', True)
+        compile_dir = settings_get('compileDir')
+        source_file = self.view.file_name()
+        source_dir = os.path.normcase(os.path.dirname(source_file))
+        relative_div = settings_get('relativeDir')
+        relative_div = os.path.normcase(relative_div) if relative_div else False
+
+        # print "Compiling: " + source_file
+        # if sys.platform == "win32":
+        #     args = ['-c', source_file.encode('utf8')]
+        # else:
+        args = ['-c', source_file]
+        if no_wrapper:
+            args = ['-b'] + args
+        if compile_dir and (isinstance(compile_dir, str) or isinstance(compile_dir, unicode)):
+            print("Compile dir specified: " + compile_dir)
+            # Check for absolute path or relative path for compile_dir
+            if not os.path.isabs(compile_dir):
+                compile_dir = os.path.join(source_dir, compile_dir)
+            elif relative_div and source_dir.startswith(relative_div):
+                compile_dir = source_dir.replace(relative_div, compile_dir, 1)
+            # create folder if not exist
+            if not os.path.exists(compile_dir):
+                os.makedirs(compile_dir)
+                print("Compile dir did not exist, created folder: " + compile_dir)
+            folder, file_nm = os.path.split(source_file)
+            args = ['--output', compile_dir] + args
+
+        result = run("coffee", args=args)
+
+        if result['okay'] is True:
+            status = 'Compilation Succeeded'
+        else:
+            errorFirstLine = result['err'].splitlines()[0]
+            status = 'Compilation FAILED '+errorFirstLine
+            sublime.error_message(errorFirstLine)
+
+        sublime.status_message(status)
+
+        later = lambda: sublime.status_message(status)
+        sublime.set_timeout(later, 300)
+
+
+class CompileAndDisplayCommand(TextCommand):
+    def is_enabled(self):
+        return isCoffee(self.view)
+
+    def run(self, edit, **kwargs):
+        no_wrapper = settings_get('noWrapper', True)
+        output = self.view.window().new_file()
+        output.set_scratch(True)
+        opt = kwargs["opt"]
+        if opt == '-p':
+            output.set_syntax_file('Packages/JavaScript/JavaScript.tmLanguage')
+        args = [opt]
+
+        if no_wrapper:
+            args = ['-b'] + args
+
+        res = brew(args, Text.get(self.view))
+        if res["okay"] is True:
+            output.insert(edit, 0, res["out"])
+        else:
+            output.insert(edit, 0, res["err"].split("\n")[0])
+
+
+class CheckSyntaxCommand(TextCommand):
+    def is_enabled(self):
+        return isCoffee(self.view)
+
+    def run(self, edit):
+        res = brew(['-b', '-p'], Text.get(self.view))
+        if res["okay"] is True:
+            status = 'Valid'
+        else:
+            status = res["err"].split("\n")[0]
+        sublime.status_message('Syntax %s' % status)
+
+
+class QuickRunBarCommand(WindowCommand):
+    def finish(self, text):
+        if text == '':
+            return
+        text = "{puts, print} = require 'util'\n" + text
+        res = brew(['-b'], text)
+        if res["okay"] is True:
+            output = self.window.new_file()
+            output.set_scratch(True)
+            output.run_command('insert', {'characters': res["out"]})
+        else:
+            sublime.status_message('Syntax %s' % res["err"].split("\n")[0])
+
+    def run(self):
+        self.window.show_input_panel('Coffee >', '', self.finish, None, None)
+
+
+class RunCakeTaskCommand(WindowCommand):
+    def finish(self, task):
+        if task == '':
+            return
+
+        if not self.window.folders():
+            cakepath = path.dirname(self.window.active_view().file_name())
+        else:
+            cakepath = path.join(self.window.folders()[0], 'Cakefile')
+            if not path.exists(cakepath):
+                cakepath = path.dirname(self.window.active_view().file_name())
+
+        if not path.exists(cakepath):
+            return sublime.status_message("Cakefile not found.")
+
+        res = cake(task, cakepath)
+        if res["okay"] is True:
+            if "No such task" in res["out"]:
+                msg = "doesn't exist"
+            else:
+                msg = "suceeded"
+        else:
+            msg = "failed"
+        sublime.status_message("Task %s - %s." % (task, msg))
+
+    def run(self):
+        self.window.show_input_panel('Cake >', '', self.finish, None, None)
+
+
+#                               _
+#   __ _ _ __   ___  _ __ __  _(_)
+#  / _` | '_ \ / _ \| '_ \\ \/ / |
+# | (_| | |_) | (_) | | | |>  <| |
+#  \__,_| .__/ \___/|_| |_/_/\_\_|
+#       |_|
+
+def watched_filename(view_id):
+    view = ToggleWatch.views[view_id]['input_obj']
+    if view.file_name() is not None:
+        filename = view.file_name().split('/')[-1]
+    else:
+        filename = "Unsaved File"
+    return filename
+
+
+class ToggleWatch(TextCommand):
+    views = {}
+    outputs = {}
+
+    def is_enabled(self):
+        return isCoffee(self.view)
+
+    def run(self, edit):
+        myvid = self.view.id()
+        if not myvid in ToggleWatch.views:
+
+            views = ToggleWatch.views
+            views[myvid] = {'watched': True, 'modified': True, 'input_closed': False}
+            views[myvid]["input_obj"] = self.view
+
+            print("Now watching", watched_filename(myvid))
+            createOut(myvid,edit)
+
+        else:
+            views = ToggleWatch.views
+
+            views[myvid]['watched'] = not views[myvid]['watched']
+            if not views[myvid]['watched']:
+                print("Stopped watching", watched_filename(myvid))
+
+            if views[myvid]['output_open'] is False:
+                print("Openning output and watching", watched_filename(myvid))
+                createOut(myvid, edit)
+
+            elif views[myvid]['watched'] is True:
+                print("Resuming watching", watched_filename(myvid))
+                refreshOut(myvid, edit)
+
+
+def cleanUp(input_view_id):
+    del ToggleWatch.outputs[ToggleWatch.views[input_view_id]['output_id']]
+    del ToggleWatch.views[input_view_id]
+    return
+
+
+def get_output_filename(input_view_id):
+    input_filename = watched_filename(input_view_id)
+    fileName, fileExtension = os.path.splitext(input_filename)
+    output_filename = fileName + '.js'
+    return output_filename
+
+
+def createOut(input_view_id,edit):
+    #create output panel and save
+    this_view = ToggleWatch.views[input_view_id]
+    outputs = ToggleWatch.outputs
+    output = this_view["input_obj"].window().new_file()
+    output.set_scratch(True)
+    output.set_syntax_file('Packages/JavaScript/JavaScript.tmLanguage')
+    this_view['output_id'] = output.id()
+    this_view["output_obj"] = output
+    this_view["output_open"] = True
+    this_view["edit"] = edit
+    # Getting file extension
+    output_filename = get_output_filename(input_view_id)
+    output.set_name(output_filename)
+
+    if not output.id() in outputs:
+        outputs[output.id()] = {'boundto': input_view_id}
+    refreshOut(input_view_id, edit)
+    return output
+
+
+def refreshOut(view_id, edit):
+    this_view = ToggleWatch.views[view_id]
+    this_view['last_modified'] = time.mktime(time.gmtime())
+    #refresh the output view
+    no_wrapper = settings_get('noWrapper', True)
+
+    args = ['-p']
+    if no_wrapper:
+        args = ['-b'] + args
+
+    res = brew(args, Text.get(this_view['input_obj']))
+    output = this_view['output_obj']
+    this_view['modified'] = False
+    current_auto_indent = output.settings().get("auto_indent")
+    output.settings().set("auto_indent", False)
+    if res["okay"] is True:
+        output.run_command('select_all')
+        output.run_command('insert', {'characters': res["out"]})
+    else:
+        output.run_command('insert', {'characters': res["err"].split("\n")[0]})
+    output.settings().set("auto_indent", current_auto_indent)
+
+    return
+
+
+def isView(view_id):
+    # are they modifying a view (rather than a panel, etc.)
+    if not view_id:
+        return False
+    window = sublime.active_window()
+    view = window.active_view() if window is not None else None
+    return (view is not None and view.id() == view_id)
+
+
+def close_output(input_id):
+    views = ToggleWatch.views
+    v = views[input_id]
+    output = v['output_obj']
+    if v['output_open'] is True:
+        output.window().focus_view(output)
+        output.window().run_command("close")
+        print(watched_filename(input_id), "was closed. Closing the Output")
+        cleanUp(input_id)
+
+    return
+
+
+class CaptureEditing(sublime_plugin.EventListener):
+
+    def handleTimeout(self, vid):
+        this_view = ToggleWatch.views[vid]
+        modified = this_view['modified']
+        if modified is True:
+            # been 1000ms since the last modification
+            refreshOut(vid,this_view["edit"])
+
+    def on_modified(self, view):
+        vid = view.id()
+        watch_modified = settings_get('watchOnModified')
+
+        if watch_modified is not False and vid in ToggleWatch.views:
+            if watch_modified is True:
+                delay = 0.5
+            elif watch_modified < 0.5:
+                delay = 0.5
+            else:
+                delay = watch_modified
+            #then we have a watched input.
+            this_view = ToggleWatch.views[vid]
+            if this_view['modified'] is False:
+                this_view['modified'] = True
+                if this_view['watched'] is True:
+                    sublime.set_timeout(functools.partial(self.handleTimeout, vid), int(delay * 1000))
+            return
+
+    def on_post_save(self, view):
+        # print "isCoffee " + str(isCoffee())
+        watch_save = settings_get('watchOnSave', True)
+        if watch_save:
+            save_id = view.id()
+            views = ToggleWatch.views
+            if save_id in views:
+                # getting view object
+                save_view = ToggleWatch.views[save_id]
+                # check if modified
+                if save_view['modified'] is True:
+                    refreshOut(save_id,save_view['edit'])
+        compile_on_save = settings_get('compileOnSave', True)
+        if compile_on_save is True and isCoffee() is True:
+
+            print("Compiling on save...")
+            view.run_command("compile")
+        show_compile_output_on_save = settings_get('showOutputOnSave', True)
+        if show_compile_output_on_save is True and isCoffee() is True and RunScriptCommand.PANEL_IS_OPEN is True:
+            print("Updating output panel...")
+            view.run_command("compile_output")
+
+        return
+
+    def on_close(self, view):
+        close_id = view.id()
+        views = ToggleWatch.views
+        if close_id in views:
+            #this is an input
+            views[close_id]['input_closed'] = True
+            close_output(close_id)
+
+        if close_id in ToggleWatch.outputs and views[ToggleWatch.outputs[close_id]['boundto']]['input_closed'] is not True:
+            #this is an output
+            boundview = ToggleWatch.outputs[close_id]['boundto']
+            thatview = views[boundview]
+            thatview['output_open'] = False
+            thatview['watched'] = False
+
+            filename = watched_filename(boundview)
+            print("The output was closed. No longer watching", filename)
+
+        return
+
+
+class RunScriptCommand(TextCommand):
+    PANEL_NAME = 'coffee_compile_output'
+    PANEL_IS_OPEN = False
+
+    def is_enabled(self):
+        return isCoffee(self.view)
+
+    def run(self, edit):
+        window = self.view.window()
+
+        #refresh the output view
+        no_wrapper = settings_get('noWrapper', True)
+
+        source_dir, source_file = path.split(self.view.file_name())
+
+        cwd = source_dir
+
+        args = [self.view.file_name()]
+        if no_wrapper:
+            args = args + ['-b']
+
+        res = brew(args, "", cwd)
+        panel = window.get_output_panel(self.PANEL_NAME)
+        panel.set_syntax_file('Packages/JavaScript/JavaScript.tmLanguage')
+        panel.set_read_only(False)
+        output = panel
+
+        if res["okay"] is True:
+            output.run_command('append', {'characters': res["out"]})
+        else:
+            output.run_command('append', {'characters': res["err"].split("\n")[0]})
+        output.sel().clear()
+        output.set_read_only(True)
+
+        window.run_command('show_panel', {'panel': 'output.%s' % self.PANEL_NAME})
+        self.PANEL_IS_OPEN = True
+        return
---
+++ packages/Better CoffeeScript/README.md
@@ -0,0 +1,329 @@
+
+	                                         __    
+	   __     _____     ___     ___    __  _/\_\   
+	 /'__`\  /\ '__`\  / __`\ /' _ `\ /\ \/'\/\ \  
+	/\ \L\.\_\ \ \L\ \/\ \L\ \/\ \/\ \\/>  </\ \ \ 
+	\ \__/.\_\\ \ ,__/\ \____/\ \_\ \_\/\_/\_\\ \_\
+	 \/__/\/_/ \ \ \/  \/___/  \/_/\/_/\//\/_/ \/_/
+	            \ \_\                              
+	             \/_/                              
+
+
+	 ____            __    __                   
+	/\  _`\         /\ \__/\ \__                
+	\ \ \L\ \     __\ \ ,_\ \ ,_\    __   _ __  
+	 \ \  _ <'  /'__`\ \ \/\ \ \/  /'__`\/\`'__\
+	  \ \ \L\ \/\  __/\ \ \_\ \ \_/\  __/\ \ \/ 
+	   \ \____/\ \____\\ \__\\ \__\ \____\\ \_\ 
+	    \/___/  \/____/ \/__/ \/__/\/____/ \/_/ 
+	                                            
+	             
+			                ___    ___                                                __      
+			              /'___\ /'___\                                    __        /\ \__   
+			  ___    ___ /\ \__//\ \__/   __     __    ____    ___   _ __ /\_\  _____\ \ ,_\  
+			 /'___\ / __`\ \ ,__\ \ ,__\/'__`\ /'__`\ /',__\  /'___\/\`'__\/\ \/\ '__`\ \ \/  
+			/\ \__//\ \L\ \ \ \_/\ \ \_/\  __//\  __//\__, `\/\ \__/\ \ \/ \ \ \ \ \L\ \ \ \_ 
+			\ \____\ \____/\ \_\  \ \_\\ \____\ \____\/\____/\ \____\\ \_\  \ \_\ \ ,__/\ \__\
+			 \/____/\/___/  \/_/   \/_/ \/____/\/____/\/___/  \/____/ \/_/   \/_/\ \ \/  \/__/
+			                                                                      \ \_\       
+			                                                                       \/_/       
+                               
+# Jump to Section
+
+* [Latest Change Log](#latest-changelog)
+* [Installation](#installation)
+* [Updating](#updating)
+* [Commands/Shortcuts](#commandsshortcuts)
+* [Snippets](#snippets)
+* [Building](#building)
+* [Settings](#settings)
+* [Special Thanks](#special-thanks)
+* [Support this Project: ![](https://www.gittip.com/assets/widgets/heart.gif) Tip Me!](https://www.gittip.com/aponxi/)
+* [Donate Me as little as you want!](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VBUW4M9LKTR62)
+
+# Overview
+
+**This package is for Sublime Text 3**. If you are looking for Sublime Text 2, then please refer to [st2 branch here](https://github.com/aponxi/sublime-better-coffeescript/tree/st2).
+
+## Description
+
+CoffeeScript plug-in was originally created by @Xavura. As I began writing a lot of code in CoffeeScript, I felt the need for side-by-side view for compiled CoffeeScript. Since Xavura's repository have been inactive I decided to branch out my own version. The biggest change in my branch is the Watch Mode which updates the compiled JavaScript view whenever you modify the CoffeeScript thus enabling you to view your progress side-by-side.
+
+I use this plug-in almost everyday! Therefore, whenever I am not developing I am in testing. I'll do my best to make sure every request or bug will be answered since I'm a frequent user.
+
+## Contributing
+
+- Please use [aponxi/issues page](https://github.com/aponxi/sublime-better-coffeescript/issues) to make requests or report bugs.
+- Please make _pull requests_ to the respective branch (`master` branch is for Sublime Text 3, `st2` branch is for Sublime Text 2.)
+
+# Installation
+
+## via Package Control
+
+> This is the recommended installation method.
+
+If you have Sublime Package Control, you know what to do. If not, well: it's a package manager for Sublime Text 2; it's awesome and you can [read about it here](http://wbond.net/sublime_packages/package_control). It is in Alpha stage for Sublime Text 3 and installation guide is [at this page](http://wbond.net/sublime_packages/package_control/installation#ST3).
+
+To install Package Control by Git:
+
+The Packages/ folder listed below refers to the folder that opens when you use the `Preferences > Browse Packages…` menu.
+```
+cd Packages/
+git clone https://github.com/wbond/sublime_package_control.git "Package Control"
+cd "Package Control"
+git checkout python3
+```
+
+After installing the package manager:
+
+* Open the Command Pallete (`ctrl+shift+P` or `cmd+shift+P`).
+* Type "Install Package" and hit return.
+* Type "Better CoffeeScript" and hit return.
+
+## via Source Control
+
+> If you plan to contribute, then you should install via this method. Otherwise it is recommended that you install the package via Package Control, see above.
+
+Sublime stores packages in the following locations:
+
+	Nix: ~/.config/sublime-text-3/packages
+	Mac: ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+	Win: %APPDATA%\Sublime Text 3\Packages
+
+### As a repository within the packages directory
+
+Open a Terminal/Console and run the following commands, replacing `PACKAGE_PATH` with the path corresponding to your OS above.
+
+	cd PACKAGE_PATH
+	git clone https://github.com/aponxi/sublime-better-coffeescript.git "Better CoffeeScript"
+
+### As a repository outside of the packages directory
+
+If you use Github for Mac/Windows which store repositories in a specific location, or if you just don't want a repository in your packages directory, then instead you can use a link.
+
+If you don't yet have the repository, then grab it via your GUI program or via the command line:
+
+	cd WHEREVER_YOU_WANT
+	git clone https://github.com/aponxi/sublime-better-coffeescript.git
+
+Once that is done, we will create the link:
+
+#### Windows:
+
+	cd PACKAGE_PATH
+	mklink /D "Better CoffeeScript" ABSOLUTE_PATH_TO_REPOSITORY
+
+#### Nix/Mac:
+
+	cd PACKAGE_PATH
+	ln -s ABSOLUTE_PATH_TO_REPOSITORY "Better CoffeeScript"
+
+#### A note on Package Control
+
+When Package Control tries to update your packages, if you have a repository in your packages directory then it will try to pull down and merge any changes. If you don't want this to happen and would rather handle everything yourself, then you can add the following to your settings (Preferences » Package Settings » Package Control » Settings - User):
+
+	"auto_upgrade_ignore": ["Better CoffeeScript"]
+
+# Updating
+
+If you are using Package Control, updating will be automatic and you don't have to worry about it.
+
+If using Source Control:
+
+	cd "PACKAGE_PATH/Better CoffeeScript"
+	git fetch origin
+	git merge origin/master
+
+# Commands/Shortcuts
+
+You can access the commands either using the command palette (`ctrl+shift+P` or `cmd+shift+P`) or via shortcuts.
+
+	alt+shift+t - Run a Cake task
+	alt+shift+r - Run some CoffeeScript (prints output to a panel on the bottom)
+	alt+shift+s - Run a syntax check
+	alt+shift+c - Compile a file
+	alt+shift+d - Display compiled JavaScript
+	alt+shift+l - Display lexer tokens
+	alt+shift+n - Display parser nodes
+	alt+shift+w - Toggle watch mode
+	alt+shift+p - Toggle output panel
+
+
+Context menu has `Compile Output` that compiles the current CoffeeScript and outputs the javascript code that is run, in a panel.
+
+**Note:** Some of the commands use the Status Bar for output, so you'll probably want to enable it (Tools » Show Status Bar).
+
+
+
+# Snippets
+
+- Use `TAB` to run a snippet after typing the trigger.
+- Use `TAB` and `shift+TAB` to cycle forward/backward through fields.
+- Use `ESC` to exit snippet mode.
+
+### Snippet Triggers
+
+**Comprehension**
+
+	Array:  forin
+	Object: forof
+	Range:  fori (inclusive)
+	Range:  forx (exclusive)
+
+**Statements**
+
+	If:        if
+	Else:      el
+	If Else:   ifel
+	Else If:   elif
+	Switch:    swi
+	Ternary:   ter
+	Try Catch: try
+	Unless:    unl
+
+**Classes**
+
+	Class - cla
+
+**Other**
+
+	Function:      -
+	Function:      = (bound)
+	Interpolation: #
+
+# Building
+
+> When using the build system, it is assumed that your `.sublime-project` file lives in your project's base directory (due to limitations with the build system).
+
+Hitting `F7` (Tools » Build) will run the Cake task 'sbuild'.
+
+If you're not quite sure what the point of this is then read on.
+
+Let's say before distributing your project that you would like to combine all of your `.js` files into one and then minify them them using UglifyJS or something.
+
+That's what this is for! You would create a `Cakefile` and inside it you would write a task:
+
+	task 'sbuild', 'Prepare project for distribution.', ->
+		# ...
+
+# Settings
+
+Go to `Preferences > Package Settings > CoffeeScript > Settings - User` to change settings.
+
+```Javascript
+{
+	/*
+		The directories you would like to include in $PATH environment variable.
+		Use this if your node installation is at a seperate location and getting errors such as `cannot find node executable`
+
+		example:
+		"envPATH": "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+	*/
+	"envPATH": "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+	/*
+		The directory containing your coffee binary. Usually
+		/usr/local/bin.
+	*/
+	"binDir": "/usr/local/bin"
+
+	/*
+		Compile without the top-level function wrapper (coffee -b).
+	*/
+
+,	"noWrapper": true
+
+	/*
+		Enable or disable refresh the compiled Output on Save.
+		Only available for watch mode.
+	*/
+,	"watchOnSave": true
+	/*
+		Enable refreshing compiled JS when CoffeeScript is modified.
+
+		Put false to disable
+		Put a number of seconds to delay the refresh
+	*/
+,	"watchOnModified": 0.5
+	/*
+		Enable Compiling on save. It will compile into the same folder.
+	*/
+,	"compileOnSave": true
+	/*
+		## Enable outputting the results of the compiled coffeescript in a panel
+	*/
+,	"showOutputOnSave": false
+	/*
+		## Enable compiling to a specific directory.
+		#### Description
+
+		if it is a string like 'some/directory' then `-o some/directory` will be added to `coffee` compiler. 
+		if it is false or not string then it will compile your `script.coffee` to the directory it is in.
+
+		#### Example:
+		Directory is relative to the file you are editting if specified such as
+			compileDir": "out"
+		Directory is absolute if specified such as
+			compileDir": "/home/logan/Desktop/out"
+		
+	*/
+,	"compileDir": false
+	/*
+		## Enable compiling to a specific relative directory.
+
+		#### Example:
+		Set absolute path for compile dir:
+			"compileDir": "/home/user/projects/js"
+		And relative folder
+			"relativeDir": "/home/user/projects/coffee"
+
+		So
+			"/home/user/projects/coffee/app.coffee" will compile to "/home/user/projects/js/app.js"
+			"/home/user/projects/coffee/models/prod.coffee" will compile to "/home/user/projects/js/models/prod.js"
+		
+	*/
+,	"relativeDir": false
+
+
+
+}
+
+
+```
+
+## Project settings
+
+Go to `Project > Edit Project` to change project settings.
+
+```Javascript
+{
+	"folders":
+	[
+		...
+	],
+	"settings":
+	{
+		"CoffeeScript":
+		{
+			"noWrapper": true,
+			"compileOnSave": true,
+			"compileDir": "out"
+		}
+	}
+}
+
+
+```
+# Latest Changelog
+### v0.7.0 01/June/2013
+
+- merged st3 with master branch
+- now the sublime text 2 support is in st2 branch
+- fixed the @ highlight in language definitions
+- fixed an error you would get when it was looking for project settings when it wasn't a project we were editing
+
+# Special Thanks
+
+* [agibsonsw](https://github.com/agibsonsw) for his help in writing WatchMode
+* [Xavura](https://github.com/Xavura) for writing the base of this plugin
+* [lavrton](https://github.com/lavrton) for his contributions 
